25f23dfec4c73e58e7e472a553f6f6e03abda1f6
AliHaidry <alihaidry11@gmail.com>
2021-02-27 15:38:02 -0500

    code update

---
+++ app.js
@@ -0,0 +1,27 @@
+/** REST APIs & HTTP Request */
+/** 
+ * What is an API?
+ * Application Programming Interface.
+ * Contract provided by one piece of software to another.
+ * Structured request and response.
+ * We just worked with an API that takes a request and responds with jokes.
+ * 
+ * What is REST APIs?
+ * Representational State Transfer.
+ * Architecture style for designing networked applications.
+ * Relies on a stateless, client-server protocol, almost always HTTP.
+ * Treats server objects as resources that can be created or destroyed.
+ * Can be used by virtually any programming language.
+ * All APIs have their own rules and structure.
+ * 
+ * HTTP Requests
+ * GET : Retrieve data from a specified resource.
+ * POST : Submit data to be processed to a specified resource.
+ * PUT : Update a specified resource.
+ * DELETE : Delete a specified resource.
+ *
+ * 
+ * HEAD : Same as get but does not return a body.
+ * OPTIONS : Returns the supported HTTP methods.
+ * PATCH : Update partial resources.
+*/
