96213487afd17375a2d1c7cda6ec98a3fb8d3399
AliHaidry <alihaidry11@gmail.com>
2021-02-27 18:48:30 -0500

    code update

--- Custom-Http-library(Ajax-with-Callbacks))/easyhttp.js
+++ Custom-Http-library(Ajax-with-Callbacks))/easyhttp.js
@@ -24,10 +24,55 @@ easyHttp.prototype.get = function(url,callback)
     this.http.send();
 }
 
+/** Make an HTTP Post Request */
+easyHttp.prototype.post = function(url,data,callback)
+{
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-Type', 'application/json');
+
+    let self = this;
+    this.http.onload= function()
+    {
+        callback(null,self.http.responseText);
+    }
+
+
+    this.http.send(JSON.stringify(data));
+}
+
+/** Make an HTTP PUT Request */
+easyHttp.prototype.put = function(url,data,callback)
+{
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-Type', 'application/json');
+
+    let self = this;
+    this.http.onload= function()
+    {
+        callback(null,self.http.responseText);
+    }
+
+
+    this.http.send(JSON.stringify(data));
+}
+
 /** Make an HTTP GET Request */
+easyHttp.prototype.delete = function(url,callback)
+{
+    this.http.open('DELETE', url, true);
 
+    let self = this;
+    this.http.onload= function()
+    {
+        if(self.http.status === 200)
+        {
+            callback(null,'Post Deleted');
+        }
+        else
+        {
+            callback('Error: '+ self.http.status);
+        }
+    }
 
-/** Make an HTTP GET Request */
-
-
-/** Make an HTTP GET Request */
+    this.http.send();
+}
