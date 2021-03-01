ebbf29e5a06e253f7c848230da548388aced9808
AliHaidry <alihaidry11@gmail.com>
2021-02-28 21:02:50 -0500

    code update

---
+++ Arrow-Functions/app.js
@@ -0,0 +1,21 @@
+/** Simple function */
+const sayHello = function ()
+{
+    console.log("Hello!");
+}
+
+sayHello();
+
+/** Arrow function */
+
+const sayGreet = () => {
+    console.log("What's up");
+}
+
+sayGreet();
+
+
+/** Arrow function more compact example */
+const bye = () => console.log("Nice to meet you!");
+
+bye();
